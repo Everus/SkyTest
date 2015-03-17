6f7d8be4b0ce5c81f62aaa30e4f6afa9ecbafa04
oper29a@gmail.com <oper29a@gmail.com>
2015-03-17 12:36:52 +0300

    Generated Entities for Student and Teacher.

---
+++ src/Sky/TestBundle/Entity/Student.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Sky\TestBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Student
+ */
+class Student
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Student
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $teachers;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->teachers = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add teachers
+     *
+     * @param \Sky\TestBundle\Entity\Teacher $teachers
+     * @return Student
+     */
+    public function addTeacher(\Sky\TestBundle\Entity\Teacher $teachers)
+    {
+        $this->teachers[] = $teachers;
+
+        return $this;
+    }
+
+    /**
+     * Remove teachers
+     *
+     * @param \Sky\TestBundle\Entity\Teacher $teachers
+     */
+    public function removeTeacher(\Sky\TestBundle\Entity\Teacher $teachers)
+    {
+        $this->teachers->removeElement($teachers);
+    }
+
+    /**
+     * Get teachers
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getTeachers()
+    {
+        return $this->teachers;
+    }
+}
---
+++ src/Sky/TestBundle/Entity/Teacher.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Sky\TestBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Teacher
+ */
+class Teacher
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Teacher
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $students;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->students = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add students
+     *
+     * @param \Sky\TestBundle\Entity\Student $students
+     * @return Teacher
+     */
+    public function addStudent(\Sky\TestBundle\Entity\Student $students)
+    {
+        $this->students[] = $students;
+
+        return $this;
+    }
+
+    /**
+     * Remove students
+     *
+     * @param \Sky\TestBundle\Entity\Student $students
+     */
+    public function removeStudent(\Sky\TestBundle\Entity\Student $students)
+    {
+        $this->students->removeElement($students);
+    }
+
+    /**
+     * Get students
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getStudents()
+    {
+        return $this->students;
+    }
+}
